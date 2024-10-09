d00b040505b11cb449e9ff74628331ee59563a57
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-10-09 10:08:43 +0200

    Get all on dab controller

---
+++ src/dab/dab.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { DabService } from './dab.service';
+import { Dab } from './entities/dab.entity';
+
+@Controller('dab')
+export class DabController {
+  constructor(private dabService: DabService) {}
+
+  @Get()
+  async findBy(@Param('id') id: number): Promise<Dab> {
+    return await this.dabService.findById(id);
+  }
+}
