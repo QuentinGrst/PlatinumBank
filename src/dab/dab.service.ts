e196db569ed114e10bf1d8fcf3cedd96ca8fa539
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-10-09 10:09:07 +0200

    addition to dab service

--- src/dab/dab.service.ts
+++ src/dab/dab.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Dab } from './entities/dab.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,35 +10,54 @@ export class DabService {
     private dabRepository: Repository<Dab>,
   ) {}
 
-  async findOne(id: number): Promise<Dab> {
+  async findById(id: number): Promise<Dab> {
     return await this.dabRepository.findOne({ where: { id: id } });
     // return `This action returns a #${id} dab`;
   }
 
-  async getUserAccounts(userId: number): Promise<any> {
-    //Promise<Account[]> {
-    return Error();
-  }
+  // async getUserAccounts(userId: number): Promise<any> {
+  //   //Promise<Account[]> {
+  //   return Error();
+  // }
 
-  async getLastTransactions(
-    accountId: number,
-    limit: number = 10,
-  ): Promise<any> {
-    return Error();
-  }
+  // async getLastTransactions(
+  //   accountId: number,
+  //   limit: number = 10,
+  // ): Promise<any> {
+  //   return Error();
+  // }
 
-  async withdrawMoney(
-    atmId: number,
-    accountId: number,
-    amount: number,
-  ): Promise<string> {
-    const atm = await this.findOne(atmId);
-    //const account = await this.accountsService.findOne(accountId);
+  // async withdrawMoney(
+  //   atmId: number,
+  //   accountId: number,
+  //   amount: number,
+  // ): Promise<string> {
+  //   const atm = await this.findOne(atmId);
+  //   const account = null; //await this.accountsService.findOne(accountId);
 
-    if (!atm) {
-      throw new NotFoundException("Le DAB n'existe pas");
-    }
+  //   if (!atm) {
+  //     throw new NotFoundException("Le DAB n'existe pas");
+  //   }
 
-    return `Withdrawal of ${amount} successful.`;
-  }
+  //   if (!account) {
+  //     throw new NotFoundException("Le compte spécifié n'existe pas");
+  //   }
+
+  //   return `Le retrait de  ${amount}€ sur le compte ${accountId} a bien été enregistré.`;
+  // }
+
+  // async transferMoney(
+  //   fromAccountId: number,
+  //   toAccountId: number,
+  //   amount: number,
+  // ) {
+  //   const fromAccount = await this.accountService.findOneBy(fromAccountId);
+  //   const toAccount = await this.accountService.findOneBy(toAccountId);
+
+  //   if (fromAccount.userId !== toAccount.user) {
+  //     throw new BadRequestException(
+  //       "Les virements ne sont possible qu'entre les comptes d'un seul et même client",
+  //     );
+  //   }
+  // }
 }
