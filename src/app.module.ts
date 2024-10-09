83fdbfd102352356d74a0d813fb235f179e19fcc
Quentin <qgransart@goweb.fr>
2024-10-09 08:13:10 +0200

    feature/add-accounts-cards-dab-transactions-users-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Account } from './accounts/account.entity';
+import { Card } from './cards/card.entity';
+import { Transaction } from './transactions/transaction.entity';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { CardsModule } from './cards/cards.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { DabModule } from './dab/dab.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'platinium_bank',
+      entities: [User, Account, Card, Transaction],
+      synchronize: true,
+    }),
+    UsersModule,
+    AccountsModule,
+    CardsModule,
+    TransactionsModule,
+    DabModule,
+  ],
 })
 export class AppModule {}
